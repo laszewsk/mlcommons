b16130b89fe45ab3192b013e45e2419b428c1f71
Robert Knuuti <uqq5zz@virginia.edu>
2022-07-09 14:43:25 -0400

    earthquake: Updated for summit LSF execution.

--- benchmarks/earthquake/latest/experiments/summit/environmenttest.py
+++ benchmarks/earthquake/latest/experiments/summit/environmenttest.py
@@ -1,5 +1,40 @@
-# which python
-# python path
-# papermill
-# jupyter
-# ....
+import os
+import platform
+import shutil
+import sys
+
+# Core tools for bootstrapping
+
+git = shutil.which("git")
+tar = shutil.which("tar")
+xz = shutil.which("xz")
+
+assert git is not None, "Git must be installed."
+assert tar is not None, "Tar must be installed."
+assert xz is not None,  "XZ-utils must be installed."
+
+# Future extension - Supporting ROCm framework.
+#assert shutil.which("rocm-smi") or \
+assert shutil.which("nvidia-smi"), "NVidia libraries must exist"
+
+
+# Validate python version
+
+python_ver = sys.version_info 
+common_ver = '.'.join(map(str, sys.version_info[:3]))
+
+assert python_ver.major >= 3, "Python must be version 3 or newer"
+assert python_ver.minor >= 7, "Python much be version 3.7 or newer"
+
+print(f"Python Path: {shutil.which('python')}")
+print(f"Python3 Path: {shutil.which('python3')}")
+print(f"Python version: {common_ver}")
+print(f"Git path: {git}")
+print(f"Tar path: {tar}")
+if platform.system().lower() == "windows":
+    usr = "USERNAME"
+else:
+    usr = "USER"
+
+print(f"Current User: {os.environ[usr]}")
+
