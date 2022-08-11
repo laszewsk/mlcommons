8b0f53d8602390b519bebf2c9eb895b8ea91ce12
Gregor von Laszewski <laszewski@gmail.com>
2022-08-10 22:08:07 -0400

    add warning if environment variable exists

--- benchmarks/earthquake/latest/experiments/ubuntu-sh/create_env.py
+++ benchmarks/earthquake/latest/experiments/ubuntu-sh/create_env.py
@@ -6,6 +6,7 @@ import yaml
 from cloudmesh.common.util import banner
 from cloudmesh.common.util import readfile
 from cloudmesh.common.util import writefile
+from cloudmesh.common.console import Console
 
 from cloudmesh.common.Shell import Shell
 import glob
@@ -19,8 +20,13 @@ for config_file in glob.glob("project/*/config.yaml"):
     config = yaml.safe_load(readfile(config_file))
     lines = []
     for key, value in config.items():
+        if key in os.environ:
+            Console.warning(f"{key} is already in env")
+        else:
+            print(".", end="")
         env = key.upper().replace(".", "_")
         lines.append(f"{env}={value}")
+    print()
     lines = "\n".join(lines)
     writefile(env_file, lines)
 
