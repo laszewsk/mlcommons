d8963c134bcf3f2e06a46e64df8bbdfac40a60b1
J.P <70083705+stapmoshun@users.noreply.github.com>
2022-10-24 14:30:11 -0400

    test env

--- benchmarks/cloudmask/target/rivanna/setup_env_and_yaml.py
+++ benchmarks/cloudmask/target/rivanna/setup_env_and_yaml.py
@@ -26,10 +26,9 @@ $ conda env list
 """
 
 # username = Shell.user()
-username = os.popen('whoami').read()
+username = os.popen('whoami').read().strip()
 print('this is it')
 print(f"sed -i 's/USERTOREPLACE/{username}/g' cloudMaskConfig.yaml")
-print('yaeh')
 os.system(f"sed -i 's/USERTOREPLACE/{username}/g' cloudMaskConfig.yaml")
 
 if "command not found" in os.popen("conda env list").read():
