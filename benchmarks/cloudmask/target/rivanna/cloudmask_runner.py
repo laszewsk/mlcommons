671079dfcad1847806c5086173b904fdc29c7dcd
J.P <70083705+stapmoshun@users.noreply.github.com>
2022-11-14 12:26:55 -0500

    fix progress import

--- benchmarks/cloudmask/target/rivanna/cloudmask_runner.py
+++ benchmarks/cloudmask/target/rivanna/cloudmask_runner.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from cloudmesh.common.Shell import Shell
-from cloudmesh.common.StopWatch import StopWatch
+from cloudmesh.common.StopWatch import progress
 
 username = Shell.user()
 path_of_mlcommons = Path(Shell.map_filename(
@@ -9,8 +9,8 @@ path_of_mlcommons = Path(Shell.map_filename(
 ).path).as_posix()
 
 os.chdir(path_of_mlcommons)
-progress = 1
-StopWatch.progress(progress)
+progress_amount = 1
+progress(progress_amount)
 gpus = ['a100', 'v100', 'p100', 'rtx2080', 'k80']
 epochs = [10, 30, 50]
 for card in gpus:
@@ -24,6 +24,6 @@ for card in gpus:
         Shell.run(
             f"sed -i '/log_file:/c\log_file: ./cloudMask_Log_{card}_{epoch}.log' cloudMaskConfig.yaml")
         Shell.run(f'sbatch --wait --gres=gpu:{card}:2 rivanna.sh')
-        progress += 6
-        StopWatch.progress(progress)
-StopWatch.progress(100)
+        progress_amount += 6
+        progress(progress_amount)
+progress(100)
