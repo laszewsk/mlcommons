Fix normalisation and accuracy calculation

benchmarks/cloudmask/data_loader.py
@@ -66,8 +66,10 @@ class SLSTRDataLoader:
             bts = handle['bts'][:]
             msk = handle['bayes'][:]
 
-        bts = (bts - 270.0) / 22.0
-        refs = refs - 0.5
+        # bts = (bts - 270.0) / 22.0
+        bts = (bts - bts.mean()) / bts.std()
+        # refs = refs - 0.5
+        refs = (refs - refs.mean()) / refs.std()
         img = np.concatenate([refs, bts], axis=-1)
 
         msk[msk > 0] = 1