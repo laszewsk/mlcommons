e279d55c96baee5af5826fabe9b361d89fc13810
Gregor von Laszewski <laszewski@gmail.com>
2023-03-10 11:29:46 -0500

    Update init.bash

--- benchmarks/cloudmask/experiments/rivanna/init.bash
+++ benchmarks/cloudmask/experiments/rivanna/init.bash
@@ -1,5 +1,5 @@
 export GITUSER=laszewsk
-export USER_SCRATCH=/scratch/$USER/github-fork
+export USER_SCRATCH=/scratch/$USER
 export PROJECT_DIR=$USER_SCRATCH/mlcommons/benchmarks/cloudmask
 export PROJECT_DATA=$USER_SCRATCH/data
 
@@ -7,6 +7,23 @@ mkdir -p $USER_SCRATCH
 mkdir -p $PROJECT_DATA
 cd $USER_SCRATCH
 
-# git clone https://github.com/$GITUSER/mlcommons.git
+git clone https://github.com/$GITUSER/mlcommons.git
 
 cd $PROJECT_DIR
+
+module purge
+module load  gcc/9.2.0  cuda/11.0.228  openmpi/3.1.6 python/3.8.8
+
+time python3 -m venv $USER_SCRATCH/ENV3
+source $USER_SCRATCH/ENV3/bin/activate
+
+pip install pip -U
+which python
+
+cd $PROJECT_DIR/experiments/rivanna
+time make requirements
+
+time make data
+
+cd $PROJECT_DIR/experiments/rivanna/
+
