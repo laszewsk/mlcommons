586672d615e2536381133bf6985ac255d0fcf783
Gregor von Laszewski <laszewski@gmail.com>
2023-04-20 15:34:08 -0400

    Update install.sh

--- benchmarks/cloudmask/experiments/rivanna/install.sh
+++ benchmarks/cloudmask/experiments/rivanna/install.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
+#
+# Using python3
+#
+#   curl -Ls http://cloudmesh.github.io/get/ubuntu19.10 | sh
+##
+# <pre>
+#! /bin/sh
 
 git clone git@github.com:laszewsk/mlcommons.git
+
+echo
+exit 0
+
+# </pre>
