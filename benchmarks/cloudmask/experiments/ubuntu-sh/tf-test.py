a09bc030f722bee6b74de10de1fe023bfa496f50
Gregor von Laszewski <laszewski@gmail.com>
2023-04-27 12:57:28 -0400

    add GPU test for tensorflow

--- benchmarks/cloudmask/experiments/ubuntu-sh/tf-test.py
+++ benchmarks/cloudmask/experiments/ubuntu-sh/tf-test.py
@@ -1,3 +1,10 @@
+from cloudmesh.common.util import banner
 import tensorflow as tf
-print(tf.test.is_gpu_available())
-tf.config.list_physical_devices("GPU").__len__() > 0
+available = tf.test.is_gpu_available()
+count = tf.config.list_physical_devices("GPU").__len__()
+
+banner("GPUs")
+
+print(f"GPU available: {available}")
+print(f"GPUs:          {count}")
+print()
