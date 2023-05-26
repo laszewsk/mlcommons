2219a63f701163a59c3c9a2c44a3b1af2ca34ea2
Gregor von Laszewski <laszewski@gmail.com>
2023-05-26 13:41:06 -0400

    add bin dir due to restriction of singularity on rivanna

---
+++ benchmarks/earthquake/apr2023/rivanna/bin/singularity-image.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+NAME=$1
+
+start_total=`date +%s`
+cp ${NAME}.def build.def
+sudo /opt/singularity/3.7.1/bin/singularity build output_image.sif build.def
+cp output_image.sif ${NAME}.sif
+# make -f Makefile clean
+end_total=`date +%s`
+time_total=$((end_total-start_total))
+echo "Time for image build:     ${time_total} s"
